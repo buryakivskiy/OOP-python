7ea086da1554df44326ade1e1edd0211cc12b2c2
buryakivskiy <buryakivskiy21@gmail.com>
2021-09-16 09:44:42 +0300

    fix issues

--- Lab 1/4.py
+++ Lab 1/4.py
@@ -1,24 +1,14 @@
-def knapsack(cap, values, weights):
-    items = []
-    for i in range(len(values)):
-        itemInfo = {
-            'vpw': values[i] / weights[i],
-            'weight': weights[i]
-        }
-        if len(items) == 0:
-            items.append(itemInfo)
-        else:
-            k = 0
-            while k < len(items) and items[k]['vpw'] > itemInfo['vpw']:
-                k += 1
-            items.insert(k, itemInfo)
-    total = 0
-    cap_left = cap
-    for item in items:
-        if cap_left - item['weight'] >= 0:
-            total += item['weight'] * item['vpw']
-            cap_left -= item['weight']
-        elif cap_left > 0:
-            total += item['vpw'] * cap_left
-            cap_left = 0
-    return total
+def knapsack(capasity, weights):
+    F = [1] + [0]*capasity
+    for i in range(len(weights)):
+        for j in range(capasity, weights[i] - 1, -1):
+            if F[j - weights[i]] == 1:
+                F[j] = 1
+    i = capasity
+    while F[i] == 0:
+        i -= 1
+    print(i)
+
+capasity = int(input())
+weights = list(map(int, input().split()))
+knapsack(capasity, weights)
